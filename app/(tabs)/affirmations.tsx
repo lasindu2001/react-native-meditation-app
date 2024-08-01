6dc141b48e018d8d15a67d64c211231067921bdd
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-08-01 10:27:50 +0530

    create affirmation index screen

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import AppGradient from '@/components/AppGradient'
+import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery'
+import GuidedAffirmationsGallery from '@/components/GuidedAffirmationsGallery'
+import { StatusBar } from 'expo-status-bar'
 
 const Affirmations = () => {
   return (
-    <View>
-      <Text>Affirmations</Text>
+    <View className="flex-1">
+      <AppGradient
+        colors={["#2e1f58", "#54426b", "#a790af"]}
+      >
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text className="text-zinc-50 text-3xl font-bold">
+            Change your beliefs with affirmations
+          </Text>
+          <View>
+            {AFFIRMATION_GALLERY.map((g) => (
+              <GuidedAffirmationsGallery
+                key={g.title}
+                title={g.title}
+                previews={g.data}
+              />
+            ))}
+          </View>
+        </ScrollView>
+      </AppGradient>
+      <StatusBar style='light' />
     </View>
   )
 }
